48eeb345228c615c46bd814169f8648965c85b2e
gmcginnis <gmcginnis@reed.edu>
2021-05-08 21:46:51 -0700

    Successful implimentation of action buttons; fixed input and output issues

--- app.r
+++ app.r
@@ -9,6 +9,7 @@ library(magrittr)
 library(ggthemes)
 library(viridis)
 library(DT)
+library(janitor)
 
 ui <- dashboardPage(
   skin = "yellow",
@@ -28,78 +29,103 @@ ui <- dashboardPage(
       tabItem(
         tabName = "tab_inputs",
         fluidRow(
-          column(width = 4,
-          box(width = NULL,
-            h3("Welcome!"),
-            h4("This dashboard will grab information from",
-            tags$a(href="https://genius.com/", "Genius"),
-            "based on provided inputs, and conduct sentiment analysis."),
-            "You can grab information for multiple albums from a single artist, or multiple albums from different artists.",
-            HTML("You can also filter out \"dropwords\" (words that might overwhelm word-by-word analysis that otherwise aren't stopwords).<br>"),
-            "Due to the nature of the function used to grab the lyric data, some songs might not have their lyric data properly loaded,
-            since Genius is not always consistent with URLs."
-          ),
-          box(width = NULL,
-            title = "Author",
-            "This dashboard was created by",
-            tags$a(href="https://github.com/gmcginnis", "Gillian McGinnis"),
-            "in May 2021 as a final project in Reed College Math 241 - Data Science."),
-          box(width = NULL, solidHeader = TRUE, background = "black", status = "warning",
+          column(
+            width = 4,
+            box(
+              width = NULL,
+              h3("Welcome!"),
+              h4("This dashboard will grab information from",
+              tags$a(href="https://genius.com/", "Genius"),
+              "based on provided inputs, and conduct sentiment analysis."),
+              "You can grab information for multiple albums from a single artist, or multiple albums from different artists.",
+              HTML("You can also filter out \"dropwords\" (words that might overwhelm word-by-word analysis that otherwise aren't stopwords).<br>"),
+              "Due to the nature of the function used to grab the lyric data, some songs might not have their lyric data properly loaded,
+              since Genius is not always consistent with URLs."
+            ),
+            box(
+              width = NULL,
+              title = "Author",
+              "This dashboard was created by",
+              tags$a(href="https://github.com/gmcginnis", "Gillian McGinnis"),
+              "in May 2021 as a final project in Reed College Math 241 - Data Science."
+            ),
+            box(
+              width = NULL, solidHeader = TRUE, background = "black", status = "warning",
               title = "Dropwords",
               textInput("input_dropwords", label = "Input dropwords, separated by commas:"),
-              actionButton(inputId = "action_dropwords",
-                           label = "Submit dropwords!",
-                           icon = icon("hand-point-right"),
-                           class = "btn-warning"),
-              h3("Top tokens"),
-              "These are the highest count non-stopwords in the data. Should some be included in the list of dropwords?",
-              tableOutput("top_tokens")
-          )),
-          column(width = 4,
-          box(width = NULL, solidHeader = TRUE, background = "black", status = "warning",
-            title = "Artist",
-            textInput("input_artist", label = "Input artist name(s):", value = "New Order, Beastie Boys"),
-            "If you are just looking at one artist's works, just put their name once.
-            If you are comparing albums from multiple artists, write them respective to their album as you list them below."
+              HTML("When you have finished inputting your dropwords, press the button below!
+                   You do not need to press the \"Get lyrics\" button again.<br><br>"),
+              actionButton(
+                inputId = "action_dropwords",
+                label = "Submit dropwords!",
+                icon = icon("hand-point-right"),
+                class = "btn-warning")
+              # h3("Top tokens"),
+              # "These are the highest count non-stopwords in the data. Should some be included in the list of dropwords?",
+              # tableOutput("output_top_tokens")
+            )
           ),
-          box(width = NULL, solidHeader = TRUE, background = "black", status = "warning",
-            title = "Albums",
-            textInput("input_albums", label = "Input albums, separated by commas:", value = "Power Corruption and Lies, Licensed To Ill"),
-            HTML("Capitalization does not matter, but spelling does.
-            Exclude commas in album titles that contain them
-            (for example, instead of inputting <i>For Emma, Forever Ago</i>, use <i>For Emma Forever Ago</i>).")
-          ),
-          actionButton(inputId = "action_grab",
-                       label = "Get lyrics!",
-                       icon = icon("hand-point-right"),
-                       class = "btn-warning"),
-          # submitButton("Get lyrics!", icon("hand-point-right")),
-          "See the BTS tab for progress."
-          ))
+          column(
+            width = 4,
+            box(
+              width = NULL, solidHeader = TRUE, background = "black", status = "warning",
+              title = "Artist",
+              textInput("input_artist", label = "Input artist name(s):", value = "New Order, Beastie Boys"),
+              "If you are just looking at one artist's works, you only need to put their name once.
+              If you are comparing albums from multiple artists, write them respective to their album as you list them below."
+            ),
+            box(
+              width = NULL, solidHeader = TRUE, background = "black", status = "warning",
+              title = "Albums",
+              textInput("input_albums", label = "Input albums, separated by commas:", value = "Power Corruption and Lies, Licensed To Ill"),
+              HTML("Capitalization does not matter, but spelling does. Exclude commas in album titles that contain them
+                   (for example, instead of inputting <i>For Emma, Forever Ago</i>, use <i>For Emma Forever Ago</i>).")
+            ),
+            box(
+              width = NULL, solidHeader = TRUE, background = "black", status = "warning",
+              HTML("Press the button below once artists and albums have been entered!
+                   The data gathering process will take a few moments. See the BTS tab for progress.<br><br>"),
+              actionButton(
+                inputId = "action_grab",
+                label = "Get lyrics!",
+                icon = icon("hand-point-right"),
+                class = "btn-warning"
+              )
+            )
+          )
+        )
       ),
       tabItem(
         tabName = "tab_bts",
         fluidRow(
-          column(width = 4,
-          box(width = NULL, solidHeader = TRUE, background = "black", status = "warning",
-            h3("Artist and albums"),
-            tableOutput("artist_album")),
-          box(width = NULL, solidHeader = TRUE, background = "black", status = "warning",
-            h3("List of dropwords:"),
-            textOutput("dropwords"))
+          column(
+            width = 4,
+            box(
+              width = NULL, solidHeader = TRUE, background = "black", status = "warning",
+              title = "Artist(s) and albums",
+              tableOutput("output_artist_album")
+            ),
+            box(
+              width = NULL, solidHeader = TRUE, background = "black", status = "warning",
+              title = "Inputted dropwords",
+              textOutput("output_dropwords"),
+              tags$hr(),
+              "If you need to add more dropwords, return to the Inputs tab and remember to press the \"Submit Dropwords\" button."
+            ),
+            box(
+              width = NULL, solidHeader = TRUE, background = "black", status = "warning",
+              title = "Top tokens",
+              "These are the top five non-stopwords in the data by album. Should some be included in the list of dropwords?",
+              tableOutput("output_top_tokens")
+            )
           ),
-          column(width = 4,
-          box(width = NULL, solidHeader = TRUE, background = "black", status = "warning",
-            h3("Dataframe of lyrics"),
-            dataTableOutput("lyrics")),
-          box(width = NULL, solidHeader = TRUE, background = "black", status = "warning",
-            h3("Top tokens"),
-            "These are the highest count non-stopwords in the data. Should some be included in the list of dropwords?",
-            tableOutput("top_tokens"))
-            #dataTableOutput("tokenized")
-            # tableOutput("albums"),
-            # textOutput("list_albums"),
-            # tableOutput("lyrics")
+          column(
+            width = 6,
+            box(
+              width = NULL, solidHeader = TRUE, status = "warning",
+              title = "Dataframe of lyrics",
+              dataTableOutput("output_lyrics")
+            )
           )
         )
       )
@@ -109,53 +135,68 @@ ui <- dashboardPage(
 
 server <- function(input, output){
   
-  observeEvent(input$action_grab, {
     
-  df_artist_album <- reactive({
+  df_artist_album <- eventReactive(input$action_grab, {
     data.frame(artist = c(unlist(str_split(input$input_artist, ", "))),
                album = c(unlist(str_split(input$input_albums, ", "))))
-    })
+  })
   
-  output$artist_album <- renderTable({df_artist_album()})
+  output$output_artist_album <- renderTable({df_artist_album()})
   
-  df_lyrics <- reactive({
-      withProgress(message = "Gathering data!",
-                   df_artist_album() %>% 
-                     add_genius(artist, album, type = "album"))
-    })
+  df_lyrics <- eventReactive(input$action_grab, {
+    withProgress(message = "Gathering data from Genius...",
+                 detail = "This will be the longest step.",
+                 df_artist_album() %>% 
+                   add_genius(artist, album, type = "album")
+    )
+  })
   
-  df_tokenized <- reactive({
+  df_tokenized <- eventReactive(input$action_grab, {
     withProgress(message = "Tokenizing data",
                  df_lyrics() %>% 
-                   unnest_tokens(output = word, input = lyric, token = "words"))
+                   unnest_tokens(output = word, input = lyric, token = "words")
+    )
   })
   
-  output$lyrics <- renderDataTable({
+  output$output_lyrics <- renderDataTable({
     datatable(style = "bootstrap",
-              df_lyrics())
+              #class = 'table-bordered table-condensed',
+              filter = list(position = "top", plain = TRUE),
+              df_lyrics() %>% 
+                select(artist, album, track_n, track_title, line, lyric) %>% 
+                mutate(
+                  artist = as.factor(artist),
+                  album = as.factor(album),
+                  track_title = as.factor(track_title)
+                ) %>% 
+                rename("No" = track_n) %>% 
+                clean_names(case = "title")
+    )
   })
   
-  # output$tokenized <- renderDataTable({
-  #   datatable(style = "bootstrap",
-  #             df_tokenized())
-  # })
-  
-  output$top_tokens <- renderTable({
+  output$output_top_tokens <- renderTable({
     df_tokenized() %>% 
       anti_join(stop_words, by = "word") %>%
+      drop_na(word) %>% 
       count(artist, album, word) %>% 
-      slice_max(order_by = n, n = 10)
+      group_by(artist, album) %>% 
+      slice_max(order_by = n, n = 5)
   })
   
+  
+  
+  # observeEvent(input$action_dropwords, {
+  #   output$dropwords <- renderText(c(unlist(str_split(input$input_dropwords, ", "))))
+  #   # df_tokenized <- reactive({df_tokenized() %>% 
+  #   #   filter(!word %in% c(unlist(str_split(input$input_dropwords, ", "))))
+  #   #   })
+  #   })
+  
+  list_drop <- eventReactive(input$action_dropwords, {
+    c(unlist(str_split(input$input_dropwords, ", ")))
   })
   
-  
-  observeEvent(input$action_dropwords, {
-    output$dropwords <- renderText(c(unlist(str_split(input$input_dropwords, ", "))))
-    # df_tokenized <- reactive({df_tokenized() %>% 
-    #   filter(!word %in% c(unlist(str_split(input$input_dropwords, ", "))))
-    #   })
-    })
+  output$output_dropwords <- renderText({list_drop()})
   
   
   
